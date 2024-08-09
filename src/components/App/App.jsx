12d37fd78cfe2f17fcc587ce00945b6735182338
Alexeev Eugene <alekseev.evgeniy.wd@yandex.ru>
2024-08-10 01:21:38 +0300

    подготовка файлов

---
+++ src/components/App/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <header className="header">
+      <h1 className="header__title">Typing Speed Trainer</h1>
+    </header>
+  );
+}
+
+export default App;
