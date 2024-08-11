3eaf836bb631c64acd9ea562d02abe2c5e06b96c
Alexeev Eugene <alekseev.evgeniy.wd@yandex.ru>
2024-08-11 17:54:11 +0300

    fix: исправить баг когда введеное слово длинее эталона

--- src/components/Word/Word.jsx
+++ src/components/Word/Word.jsx
@@ -4,19 +4,32 @@ import Letter from "../Letter/Letter"
 const Word = (props) => {
 	const { typingWord , correctWord, isTypingWordIxist, isTypingWordLonger } = props
   const correctLetterArray = correctWord.split('')
+  const correctWordLength = correctWord.length
+  console.log(correctWordLength, typingWord.slice(correctWordLength))
   return (
     <div className="word">
       {
         correctLetterArray.map((correctLetter, index) => (
           <Letter
             correctLetter={correctLetter}
-            typingLetter={isTypingWordLonger ? typingWord.slice(index) : typingWord[index]}
+            typingLetter={typingWord[index]}
             isTypingWordIxist={isTypingWordIxist}
             isTypingLetterIxist={Boolean(typingWord[index])}
             isCorrectLetter={typingWord[index] === correctWord[index]}
             key={index}
           />
         ))
+      }
+      {
+        isTypingWordLonger &&
+          <Letter
+            correctLetter={''}
+            typingLetter={typingWord.slice(correctWordLength)}
+            isTypingWordIxist={isTypingWordIxist}
+            isTypingLetterIxist={true}
+            isCorrectLetter={false}
+            key={correctWordLength}
+          />
       }
     </div>
   )
