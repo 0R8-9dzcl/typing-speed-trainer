82871e385f6550b525dc41376df9d1c6fc4863dc
Alexeev Eugene <alekseev.evgeniy.wd@yandex.ru>
2024-08-19 20:02:58 +0300

    feat: Заблокировать перемещение каретки и выделение текста, да и вообще любое взаимодействие с инпутом(кроме ввода), так как он будет скрыт от пользователя, вставку текста тоже заблокировать

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,7 +1,38 @@
+import { useEffect } from 'react'
 import './Input.css'
 
 const Input = (props) => {
-  const { className, onChange, inputRef, disabled } = props
+  const { className, onChange, inputRef, disabled: isInputDisabled } = props
+
+  const handlePreventDefault = (event) => {
+    event.preventDefault()
+  }
+  const handleKeyDown = (event) => {
+    const eventButtonCode = event.code
+    const forbiddenButtonCodes = [
+      'ArrowLeft',
+      'ArrowRight',
+      'ArrowUp',
+      'ArrowDown',
+    ]
+    const isForbiddenButton = forbiddenButtonCodes.some(
+      (forbiddenButtonCode) => forbiddenButtonCode === eventButtonCode,
+    )
+    if (isForbiddenButton) {
+      event.preventDefault()
+    }
+  }
+
+  useEffect(() => {
+    const inputElement = inputRef.current
+    inputElement.addEventListener('selectstart', handlePreventDefault)
+    inputElement.addEventListener('mousedown', handlePreventDefault)
+
+    return () => {
+      inputElement.removeEventListener('selectstart', handlePreventDefault)
+      inputElement.removeEventListener('selectstart', handlePreventDefault)
+    }
+  }, [inputRef])
   return (
     <input
       type="text"
@@ -11,7 +42,9 @@ const Input = (props) => {
       autoCorrect="off"
       onChange={onChange}
       ref={inputRef}
-      disabled={disabled}
+      disabled={isInputDisabled}
+      onPaste={handlePreventDefault}
+      onKeyDown={handleKeyDown}
     />
   )
 }
